6a6dca58b47d3d2137aaebbaab4ba2e83dea32f6
OttoDyva <cph-ot@cphbusiness.dk>
2023-10-27 12:17:19 +0200

    Ny klasse

---
+++ src/main/java/KlasseOtto.java
@@ -0,0 +1,12 @@
+public class KlasseOtto
+{
+    String otto = "Mit navn er otto";
+    public KlasseOtto(String otto)
+    {
+        this.otto = otto;
+    }
+    public String toString()
+    {
+        return otto;
+    }
+}
